ca70e6b0d8341524e7af1b9d336b930020a40f06
Ana Arango <anaarango@Anas-iMac.local>
2022-10-16 21:08:35 -0700

    working on createpost page - October 16

--- rooty-next/pages/CreatePost.jsx
+++ rooty-next/pages/CreatePost.jsx
@@ -1,4 +1,9 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+import Text from "../components/text";
+import { FlexBox, Wrapper } from "../styles/globals";
+import Input, {TextIn} from "../components/input"; 
+import { TextInput } from "../components/input";
+import Button from "../components/button";
 
 export default function CreatePost(props) {
     const [file, setFile] = useState()
@@ -19,39 +24,61 @@ export default function CreatePost(props) {
 
     return (
         <>
-            <main>
-                <form>
-                    <div>
-                        <h1>Create your post</h1>
-                        <button onClick={(e) => handleSubmit(e)} type="submit">Done</button>
-                    </div>
+            <Wrapper justifyContent="flex-start" alignItems="flex-start" dir="column">
+                <Text txt="Create your post" size="24px" weight="bold" padding="40px 0px 10px 20px"></Text>
+                <FlexBox padding="20px 0px 20px 20px" border="0.5px solid #545454" width="100vw" justifyContent="flex-start">
+                    <Input type="file"></Input>
+                </FlexBox>
+                <FlexBox border="0.5px solid #545454" width="100vw" justifyContent="flex-start" alignItems="flex-start" dir="column" padding="0px 0px 15px 0px">
+                    <Text txt="Choose a Category" size="16px" weight="bold" padding="15px 0px 20px 20px"></Text>
+                    <FlexBox flexWrap="wrap">
+                        <Button txt="Broadcast & Media" border="solid 2px #545454" bgColor="white" color="#545454" width="fit-content" padding="15px"></Button>
+                        <Button txt="Business & Finance" border="solid 2px #545454" bgColor="white" color="#545454" width="fit-content" padding="15px"></Button>
+                        <Button txt="Computing" border="solid 2px #545454" bgColor="white" color="#545454" width="fit-content" padding="15px"></Button>
+                        <Button txt="Digital Arts & Media" border="solid 2px #545454" bgColor="white" color="#545454" width="fit-content" padding="15px"></Button>
+                        <Button txt="Marketing" border="solid 2px #545454" bgColor="white" color="#545454" width="fit-content" padding="15px"></Button>
+                        <Button txt="Tutoring" border="solid 2px #545454" bgColor="white" color="#545454" width="fit-content" padding="15px"></Button>
+                    </FlexBox>
+                </FlexBox>
+                <FlexBox border="0.5px solid #545454" width="100vw" justifyContent="flex-start" alignItems="flex-start" dir="column" padding="0px 0px 15px 0px">
+                    <Text txt="Type Keywords" size="16px" weight="bold" padding="15px 0px 20px 20px"></Text>
+                    <Input type="search" bgColor="white" border="solid 1px #545454" margin="0px 0px 0px 20px"></Input>
+                </FlexBox>
+                <FlexBox border="0.5px solid #545454" width="100vw" justifyContent="flex-start" alignItems="flex-start" dir="column" padding="0px 0px 15px 0px">
+                    <Text txt="Post Title" size="16px" weight="bold" padding="15px 0px 20px 20px"></Text>
+                    <Input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+                </FlexBox>
+                <FlexBox border="0.5px solid #545454" width="100vw" justifyContent="flex-start" alignItems="flex-start" dir="column" padding="0px 0px 15px 0px">
+                    <Text txt="Describe your Service" size="16px" weight="bold" padding="15px 0px 20px 20px"></Text>
+                    <TextInput value={description} onChange={(e) => setDescription(e.target.value)}  type="textarea" />
+                </FlexBox>
+                
+                        
+                
+                {/* <TextInput type="textarea"></TextInput> */}
+                {/* <button onClick={(e) => handleSubmit(e)} >+</button>
 
-                    <input type="file"></input>
-                    <button onClick={(e) => handleSubmit(e)} >+</button>
+                <button onClick={(e) => handleSubmit(e)} >Choose a category</button>
+                <h3>Choose keyword</h3>
+                <button onClick={(e) => handleSubmit(e)} >Audio Mix</button>
+                <button onClick={(e) => handleSubmit(e)} >Web Design</button>
+                <button onClick={(e) => handleSubmit(e)} >Logo Design</button> */}
+                {/* <label> Write your title
+                    <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" />
+                </label> */}
 
-                    <button onClick={(e) => handleSubmit(e)} >Choose a category</button>
-                    <h3>Choose keyword</h3>
-                    <button onClick={(e) => handleSubmit(e)} >Audio Mix</button>
-                    <button onClick={(e) => handleSubmit(e)} >Web Design</button>
-                    <button onClick={(e) => handleSubmit(e)} >Logo Design</button>
-                    <label> Write your title
-                        <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" />
-                    </label>
+                <label> Describe your service
+                    <input value={description} onChange={(e) => setDescription(e.target.value)}  type="text" />
+                </label>
 
-                    <label> Describe your service
-                        <input value={description} onChange={(e) => setDescription(e.target.value)}  type="text" />
-                    </label>
-
-                    <input name="BarterOrCash" id="Barter" type="radio" value={"barter"} onClick={() => setIsBarter(true)} />
-                    <label htmlFor="Barter"> Barter
-                    </label>
-                        <input name="BarterOrCash" id="Cash" type="radio" value={"cash"} onClick={() => setIsBarter(false)}/>
-                    <label htmlFor="Cash"> Cash
-                    </label>
-
-
-                </form>
-            </main>
+                <input name="BarterOrCash" id="Barter" type="radio" value={"barter"} onClick={() => setIsBarter(true)} />
+                <label htmlFor="Barter"> Barter
+                </label>
+                    <input name="BarterOrCash" id="Cash" type="radio" value={"cash"} onClick={() => setIsBarter(false)}/>
+                <label htmlFor="Cash"> Cash
+                </label>
+                <button onClick={(e) => handleSubmit(e)} type="submit">Submit</button>
+            </Wrapper>
         </>
     )
 }
---
+++ rooty-next/styles/globals.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const FlexBox = styled.div`
+    display: ${props=>props.display || "flex"};
+    justify-content:${props=>props.justifyContent || "center"};
+    align-items:${props=>props.alignItems || "center"};
+    flex-direction:${props=>props.dir || "row"};
+    flex-wrap: ${props=>props.flexWrap};
+    width: ${props=>props.width};
+    height: ${props=>props.height};
+    background-color: ${props=>props.bgColor};
+    padding: ${props=>props.padding};
+    margin: ${props=>props.margin || "0px"};
+    background-image: ${props=>props.linearGradient}, url(${props=>props.bgImage});
+    background-position: ${props=>props.bgPosition || "center"};
+    background-size: ${props=>props.bgPosition || "cover"};
+    border-radius:${props=>props.borderRadius};
+    filter: ${props=>props.filter};
+    border-top: ${props=>props.border};
+    z-index:${props=>props.zIndex};
+    position: ${props=>props.position};
+    top: ${props=>props.top};
+    left:${props=>props.left};
+    box-shadow: ${props=>props.boxShadow};
+`
+
+export const Wrapper = styled(FlexBox) `
+    width: 100vw;
+    height: ${props=>props.height} || "fit-content";
+    padding:0px 0px 55px 0px
+`
+
+export const HorizontalScrollContainer = styled(FlexBox)`
+    overflow-y: scroll;
+    width: 100vw;
+    scrollbar-width: none;
+`
+
+export const ImgPlaceholder = styled.div`
+    width: ${props=>props.width || "134px"};
+    height: ${props=>props.height || "154px"};
+    border-radius: ${props=>props.borderRadius || "8px Opx Opx 8px"};
+    background-image: url(${props=>props.bgImage});
+    margin: ${props=>props.margin};
+    background-position: center;
+    background-size: cover;
+`
